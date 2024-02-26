6f16b2c39e83540703d8cd78481e8fc54c9fe1f0
Atousagh <110605688+Atousagh@users.noreply.github.com>
2024-02-26 13:58:12 +0100

    Update streamlit_app.py
    fixed typo in line 34 Jason_normalized to json_normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 # take the JSON version of the response and normalize it
-fruityvice_normalized = pandas.jason_normalized(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output the screen as a table
 streamlit.dataframe(fruityvice_normalized)
