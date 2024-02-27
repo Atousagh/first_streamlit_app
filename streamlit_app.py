ced44158ad114b560cdc2a43398fe4a7853d899d
Atousagh <110605688+Atousagh@users.noreply.github.com>
2024-02-27 11:22:26 +0100

    Update streamlit_app.py
    Added a Function and Button to Add the Fruit Name Submissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,6 @@ try:
 except URLError as e:
   streamlit.error
 
-
-
-
-
-
 streamlit.header("The fruit load list contains:")
 
 # Snowflake related functions
@@ -68,13 +63,26 @@ if streamlit.button('Get Fruit Load List'):
 
 
 
+
+
+# Allow the end user to add a fruit to the list
+def insert_row_snawflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding" + new_fruit
+    
+add_my_fruit =streamlit.text_input('What fruit would you like to add?')
+if streamlit.button ('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
+
+
+
 # Do not run annything past here while we troubleshoot.
 streamlit.stop()
 
-# add the second text entery box
-add_my_fruit =streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding', add_my_fruit)
-
 # This will not work correctly now. Adde a value to the table
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
