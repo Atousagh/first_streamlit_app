a66d9c40d8415f7e652b3f6e898360b9ce8fdefa
Atousagh <110605688+Atousagh@users.noreply.github.com>
2024-02-27 09:42:44 +0100

    Update streamlit_app.py
    fixed typo my_cure to my_cur

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,5 @@ add_my_fruit =streamlit.text_input('What fruit would you like to add?', 'jackfru
 streamlit.write('Thanks for adding', add_my_fruit)
 
 # This will not work correctly now. Adde a value to the table
-#my_cure.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
